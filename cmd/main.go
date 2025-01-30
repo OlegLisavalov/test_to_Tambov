4ef91fc1878c75c66ec0b3b1e8cc4adb1d510ccd
oleglisavalov <leg.lis@mail.ru>
2025-01-30 14:28:58 +0300

    С горем пополам всё

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"log"
 	"test_Task_New_server/config"
 	"test_Task_New_server/database"
+	"test_Task_New_server/handlers"
+	"test_Task_New_server/logger"
+	"test_Task_New_server/repository"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	logger.InitLogger()
+
 	cfg := config.LoadConfig()
-	database.ConnectDB(cfg)
+
+	db := database.ConnectDB(cfg)
+
+	newsRepo := repository.NewNewsRepository(db)
+	newsHandler := handlers.NewNewsHandler(newsRepo)
+
+	app := fiber.New()
+
+	app.Put("/news/:id", newsHandler.UpdateNews)
+	app.Get("/news", newsHandler.GetAllNews)
+	app.Get("/news/:id", newsHandler.GetNewsById) 
+
+	log.Fatal(app.Listen(":8080"))
 }
